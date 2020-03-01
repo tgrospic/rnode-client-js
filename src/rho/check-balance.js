b2294f42368ed6a70c8e318867a826aef3e2717b
Tomislav Grospic <grospic@gmail.com>
2020-03-01 23:33:40 +0100

    Update transfer and balance Rholang templates

--- src/rho/check-balance.js
+++ src/rho/check-balance.js
@@ -1,40 +1,14 @@
 // Rholang code to check REVs balance
-// https://github.com/rchain/rchain/blob/3eca061/rholang/examples/vault_demo/2.check_balance.rho
-//
-// The code is modified to send balance on `rho:rchain:deployId` name.
-export const checkBalance_rho = revAddress => `
-  new
-    rl(\`rho:registry:lookup\`), RevVaultCh,
-    vaultCh, balanceCh,
-    deployId(\`rho:rchain:deployId\`),
-    stdout(\`rho:io:stdout\`)
-  in {
-
+// - intended for use with RNode exploratory deploy
+export const checkBalance_rho = addr => `
+  new return, rl(\`rho:registry:lookup\`), RevVaultCh, vaultCh in {
     rl!(\`rho:rchain:revVault\`, *RevVaultCh) |
     for (@(_, RevVault) <- RevVaultCh) {
-
-      stdout!(("2.check_balance.rho")) |
-
-      match "${revAddress}" {
-        revAddress => {
-
-          stdout!(("Accessing vault at RevAddress", revAddress)) |
-
-          // most RevVault methods return an \`Either[String, A] = (false, String) \/ (true, A)\`
-          @RevVault!("findOrCreate", revAddress, *vaultCh) |
-          for (@(true, vault) <- vaultCh) {
-
-            stdout!("Obtained vault, checking balance") |
-
-            @vault!("balance", *balanceCh) |
-            for (@balance <- balanceCh) {
-
-              stdout!(("Balance is", balance)) |
-
-              deployId!(balance)
-            }
-          }
-
+      @RevVault!("findOrCreate", "${addr}", *vaultCh) |
+      for (@maybeVault <- vaultCh) {
+        match maybeVault {
+          (true, vault) => @vault!("balance", *return)
+          (false, err)  => return!(err)
         }
       }
     }
--- src/rho/transfer-funds.js
+++ src/rho/transfer-funds.js
@@ -1,36 +1,37 @@
 // Rholang code to transfer REVs
 // https://github.com/rchain/rchain/blob/3eca061/rholang/examples/vault_demo/3.transfer_funds.rho
 export const transferFunds_rho = (revAddrFrom, revAddrTo, amount) => `
-  new
-    rl(\`rho:registry:lookup\`), RevVaultCh,
-    stdout(\`rho:io:stdout\`)
-  in {
-
+  new rl(\`rho:registry:lookup\`), RevVaultCh in {
     rl!(\`rho:rchain:revVault\`, *RevVaultCh) |
     for (@(_, RevVault) <- RevVaultCh) {
-
-      stdout!(("3.transfer_funds.rho")) |
-
-      match ("${revAddrFrom}", "${revAddrTo}", ${amount}) {
-        (from, to, amount) => {
-
-          new vaultCh, revVaultkeyCh, deployerId(\`rho:rchain:deployerId\`) in {
-            @RevVault!("findOrCreate", from, *vaultCh) |
+      new vaultCh, vaultTo, revVaultkeyCh,
+        deployerId(\`rho:rchain:deployerId\`),
+        deployId(\`rho:rchain:deployId\`)
+      in {
+        match ("${revAddrFrom}", "${revAddrTo}", ${amount}) {
+          (revAddrFrom, revAddrTo, amount) => {
+            @RevVault!("findOrCreate", revAddrFrom, *vaultCh) |
+            @RevVault!("findOrCreate", revAddrTo, *vaultTo) |
             @RevVault!("deployerAuthKey", *deployerId, *revVaultkeyCh) |
-            for (@(true, vault) <- vaultCh; key <- revVaultkeyCh) {
-
-              stdout!(("Beginning transfer of ", amount, "REV from", from, "to", to)) |
-
-              new resultCh in {
-                @vault!("transfer", to, amount, *key, *resultCh) |
-                for (@result <- resultCh) {
-
-                  stdout!(("Finished transfer of ", amount, "REV to", to, "result was:", result))
+            for (@vault <- vaultCh; key <- revVaultkeyCh; _ <- vaultTo) {
+              match vault {
+                (true, vault) => {
+                  new resultCh in {
+                    @vault!("transfer", revAddrTo, amount, *key, *resultCh) |
+                    for (@result <- resultCh) {
+                      match result {
+                        (true , _  ) => deployId!((true, "Transfer successful."))
+                        (false, err) => deployId!((false, err))
+                      }
+                    }
+                  }
+                }
+                err => {
+                  deployId!((false, "REV vault cannot be found or created."))
                 }
               }
             }
           }
-
         }
       }
     }
