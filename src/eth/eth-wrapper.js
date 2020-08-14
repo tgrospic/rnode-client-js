d903275c6394a42e4ac94272214874b6d08d1029
Tomislav Grospic <grospic@gmail.com>
2020-08-14 19:03:53 +0200

    Update ETH wrapper (Metamask)

--- src/eth/eth-wrapper.js
+++ src/eth/eth-wrapper.js
@@ -1,36 +1,41 @@
 // Metamask wrapper for Ethereum provider
 // https://metamask.github.io/metamask-docs/guide/ethereum-provider.html#methods-new-api
+// Updated by EIP-1193 (ethereum.request)
+// https://eips.ethereum.org/EIPS/eip-1193
 
 // Ethereum object injected by Metamask
 const eth_ = window.ethereum
 
 export const ethDetected = !!eth_
 
+// https://docs.metamask.io/guide/ethereum-provider.html#properties
+if (ethDetected) eth_.autoRefreshOnNetworkChange = false
+
 // Send a request to Ethereum API (Metamask)
-const ethSend = (method, args) => {
+const ethRequest = (method, args) => {
   if (!eth_) throw Error(`Ethereum (Metamask) not detected.`)
 
-  return eth_.send(method, args).then(x => x.result)
+  return eth_.request({method, ...args})
 }
 
-// Request an address from the user
+// Request an address selected in Metamask
 // - the first request will ask the user for permission
-// - in Metamask settings the connection can be deleted
 export const ethereumAddress = async () => {
-  const accounts = await ethSend('eth_requestAccounts')
+  const accounts = await ethRequest('eth_requestAccounts')
 
   if (!Array.isArray(accounts))
-    throw Error(`Ethereum RPC response is not a list of accounts.`)
+    throw Error(`Ethereum RPC response is not a list of accounts (${accounts}).`)
 
   // Returns ETH address in hex format
   return accounts[0]
 }
 
 // Ethereum personal signature
-export const ethereumSign = async (data, ethAddr) => {
+// https://github.com/ethereum/go-ethereum/wiki/Management-APIs#personal_sign
+export const ethereumSign = async (bytes, ethAddr) => {
   // Create args, fix arrays/buffers
-  const args = [[...data], ethAddr]
+  const args = { params: [[...bytes], ethAddr] }
 
   // Returns signature in hex format
-  return await ethSend('personal_sign', args)
+  return await ethRequest('personal_sign', args)
 }
