4ec4dac9314d5971187def2a3a64309a1d29c96b
Tomislav Grospic <grospic@gmail.com>
2021-03-10 11:20:24 +0100

    Remove leading whitespaces in transfer contract (Trezor fix)
    close #22

--- src/rho/transfer-funds.js
+++ src/rho/transfer-funds.js
@@ -1,39 +1,44 @@
-// Rholang code to transfer REVs
-// https://github.com/rchain/rchain/blob/3eca061/rholang/examples/vault_demo/3.transfer_funds.rho
+/**
+ * Rholang code to transfer REVs
+ * https://github.com/rchain/rchain/blob/3eca061/rholang/examples/vault_demo/3.transfer_funds.rho
+ *
+ * NOTE: Leading whitespaces are removed to fix strange bug in Trezor<->Metamask communication.
+ * https://github.com/tgrospic/rnode-client-js/issues/22
+ */
 export const transferFunds_rho = (revAddrFrom, revAddrTo, amount) => `
-  new rl(\`rho:registry:lookup\`), RevVaultCh in {
-    rl!(\`rho:rchain:revVault\`, *RevVaultCh) |
-    for (@(_, RevVault) <- RevVaultCh) {
-      new vaultCh, vaultTo, revVaultkeyCh,
-        deployerId(\`rho:rchain:deployerId\`),
-        deployId(\`rho:rchain:deployId\`)
-      in {
-        match ("${revAddrFrom}", "${revAddrTo}", ${amount}) {
-          (revAddrFrom, revAddrTo, amount) => {
-            @RevVault!("findOrCreate", revAddrFrom, *vaultCh) |
-            @RevVault!("findOrCreate", revAddrTo, *vaultTo) |
-            @RevVault!("deployerAuthKey", *deployerId, *revVaultkeyCh) |
-            for (@vault <- vaultCh; key <- revVaultkeyCh; _ <- vaultTo) {
-              match vault {
-                (true, vault) => {
-                  new resultCh in {
-                    @vault!("transfer", revAddrTo, amount, *key, *resultCh) |
-                    for (@result <- resultCh) {
-                      match result {
-                        (true , _  ) => deployId!((true, "Transfer successful (not yet finalized)."))
-                        (false, err) => deployId!((false, err))
-                      }
-                    }
-                  }
-                }
-                err => {
-                  deployId!((false, "REV vault cannot be found or created."))
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+new rl(\`rho:registry:lookup\`), RevVaultCh in {
+rl!(\`rho:rchain:revVault\`, *RevVaultCh) |
+for (@(_, RevVault) <- RevVaultCh) {
+new vaultCh, vaultTo, revVaultkeyCh,
+deployerId(\`rho:rchain:deployerId\`),
+deployId(\`rho:rchain:deployId\`)
+in {
+match ("${revAddrFrom}", "${revAddrTo}", ${amount}) {
+(revAddrFrom, revAddrTo, amount) => {
+@RevVault!("findOrCreate", revAddrFrom, *vaultCh) |
+@RevVault!("findOrCreate", revAddrTo, *vaultTo) |
+@RevVault!("deployerAuthKey", *deployerId, *revVaultkeyCh) |
+for (@vault <- vaultCh; key <- revVaultkeyCh; _ <- vaultTo) {
+match vault {
+(true, vault) => {
+new resultCh in {
+@vault!("transfer", revAddrTo, amount, *key, *resultCh) |
+for (@result <- resultCh) {
+match result {
+(true , _  ) => deployId!((true, "Transfer successful (not yet finalized)."))
+(false, err) => deployId!((false, err))
+}
+}
+}
+}
+err => {
+deployId!((false, "REV vault cannot be found or created."))
+}
+}
+}
+}
+}
+}
+}
+}
 `
