dd31c28583ae1c3e30b8db1e3ba45284503d74d1
Tomislav Grospic <grospic@gmail.com>
2022-04-10 12:06:16 +0200

    Networks information updated with shard ID

--- src/rchain-networks.js
+++ src/rchain-networks.js
@@ -2,26 +2,31 @@
 const defaultPorts    = { grpc: 40401, http: 40403, httpAdmin: 40405 }
 const defaultPortsSSL = { grpc: 40401, https: 443, httpAdmin: 40405 }
 
+// Shard IDs
+const defaultShardId = 'root'
+const testNetShardId = '' // not used until after v0.13.0-alpha
+const mainNetShardId = '' // not used until HF2
+
 // Local network
 
 export const localNet = {
   title: 'Local network',
   name: 'localnet',
   hosts: [
-    { domain: 'localhost', ...defaultPorts },
-    { domain: 'localhost', grpc: 40411, http: 40413, httpAdmin: 40415 },
-    { domain: 'localhost', grpc: 40421, http: 40423, httpAdmin: 40425 },
-    { domain: 'localhost', grpc: 40431, http: 40433, httpAdmin: 40435 },
-    { domain: 'localhost', grpc: 40441, http: 40443, httpAdmin: 40445 },
-    { domain: 'localhost', grpc: 40451, http: 40453, httpAdmin: 40455 },
+    { domain: 'localhost', shardId: defaultShardId, ...defaultPorts },
+    { domain: 'localhost', shardId: defaultShardId, grpc: 40411, http: 40413, httpAdmin: 40415 },
+    { domain: 'localhost', shardId: defaultShardId, grpc: 40421, http: 40423, httpAdmin: 40425 },
+    { domain: 'localhost', shardId: defaultShardId, grpc: 40431, http: 40433, httpAdmin: 40435 },
+    { domain: 'localhost', shardId: defaultShardId, grpc: 40441, http: 40443, httpAdmin: 40445 },
+    { domain: 'localhost', shardId: defaultShardId, grpc: 40451, http: 40453, httpAdmin: 40455 },
   ],
   readOnlys: [
-    { domain: 'localhost', ...defaultPorts },
-    { domain: 'localhost', grpc: 40411, http: 40413, httpAdmin: 40415 },
-    { domain: 'localhost', grpc: 40421, http: 40423, httpAdmin: 40425 },
-    { domain: 'localhost', grpc: 40431, http: 40433, httpAdmin: 40435 },
-    { domain: 'localhost', grpc: 40441, http: 40443, httpAdmin: 40445 },
-    { domain: 'localhost', grpc: 40451, http: 40453, httpAdmin: 40455 },
+    { domain: 'localhost', shardId: defaultShardId, ...defaultPorts },
+    { domain: 'localhost', shardId: defaultShardId, grpc: 40411, http: 40413, httpAdmin: 40415 },
+    { domain: 'localhost', shardId: defaultShardId, grpc: 40421, http: 40423, httpAdmin: 40425 },
+    { domain: 'localhost', shardId: defaultShardId, grpc: 40431, http: 40433, httpAdmin: 40435 },
+    { domain: 'localhost', shardId: defaultShardId, grpc: 40441, http: 40443, httpAdmin: 40445 },
+    { domain: 'localhost', shardId: defaultShardId, grpc: 40451, http: 40453, httpAdmin: 40455 },
   ]
 }
 
@@ -34,6 +39,7 @@ const getTestNetUrls = n => {
   return {
     domain: `${instance}.testnet.rchain.coop`,
     instance,
+    shardId: testNetShardId,
     ...defaultPortsSSL,
   }
 }
@@ -45,9 +51,7 @@ export const testNet = {
   name: 'testnet',
   hosts: testnetHosts,
   readOnlys: [
-    { domain: 'observer.testnet.rchain.coop', instance: 'observer', ...defaultPortsSSL },
-    // Jim's read-only node
-    { domain: 'rnode1.rhobot.net', ...defaultPortsSSL },
+    { domain: 'observer.testnet.rchain.coop', instance: 'observer', shardId: testNetShardId, ...defaultPortsSSL },
   ],
 }
 
@@ -55,6 +59,7 @@ export const testNet = {
 
 const getMainNetUrls = n => ({
   domain: `node${n}.root-shard.mainnet.rchain.coop`,
+  shardId: mainNetShardId,
   ...defaultPortsSSL,
 })
 
@@ -66,14 +71,14 @@ export const mainNet = {
   hosts: mainnetHosts,
   readOnlys: [
     // Load balancer (not gRPC) server for us, asia and eu servers
-    { domain: 'observer.services.mainnet.rchain.coop', https: 443 },
-    { domain: 'observer-us.services.mainnet.rchain.coop', ...defaultPortsSSL },
-    { domain: 'observer-asia.services.mainnet.rchain.coop', ...defaultPortsSSL },
-    { domain: 'observer-eu.services.mainnet.rchain.coop', ...defaultPortsSSL },
+    { domain: 'observer.services.mainnet.rchain.coop', shardId: mainNetShardId, https: 443 },
+    { domain: 'observer-us.services.mainnet.rchain.coop', shardId: mainNetShardId, ...defaultPortsSSL },
+    { domain: 'observer-asia.services.mainnet.rchain.coop', shardId: mainNetShardId, ...defaultPortsSSL },
+    { domain: 'observer-eu.services.mainnet.rchain.coop', shardId: mainNetShardId, ...defaultPortsSSL },
   ],
 }
 
-export const getNodeUrls = ({name, domain, grpc, http, https, httpAdmin, httpsAdmin, instance}) => {
+export const getNodeUrls = ({name, shardId, domain, grpc, http, https, httpAdmin, httpsAdmin, instance}) => {
   const scheme       = !!https ? 'https' : !!http ? 'http' : ''
   const schemeAdmin  = !!httpsAdmin ? 'https' : !!httpAdmin ? 'http' : ''
   const httpUrl      = !!https || !!http ? `${scheme}://${domain}:${https || http}` : void 8
@@ -82,10 +87,11 @@ export const getNodeUrls = ({name, domain, grpc, http, https, httpAdmin, httpsAd
 
   return {
     network      : name,
+    shardId,
     grpcUrl,
     httpUrl,
     httpAdminUrl,
-    statusUrl    : `${httpUrl}/status`,
+    statusUrl    : `${httpUrl}/api/status`,
     getBlocksUrl : `${httpUrl}/api/blocks`,
     // Testnet only
     logsUrl : instance && `http://${domain}:8181/logs/name:${instance}`,
